5d6aecc4f83dedcb6dedf0102b28f2b8290763e6
Umair Abdullah <talhaejaz@outlook.com>
2022-10-24 20:45:36 +0500

    hero component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Header } from "./components";
+import { Header, Hero } from "./components";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export * from "./Header/Header.jsx";
+export * from "./Hero/Hero.jsx";
